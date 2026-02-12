cf2782fa2ff09e8761b5879ef4c99a9076cb15d9
fg7000 <faryar.ghazanfari@gmail.com>
2026-02-12 02:33:59 -0800

    Import NextResponse in middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,6 @@
 // import { updateSession } from "@/lib/supabase/middleware";
 import type { NextRequest } from "next/server";
-
+import { NextResponse } from "next/server";
 export async function middleware(request: NextRequest) {
 //   return await updateSession(request);
     return NextResponse.next();
